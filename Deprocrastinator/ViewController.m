f5a93747508aca1634a618e69132e337791ce064
Andy Palka <andrewmpalka@gmail.com>
2016-01-18 15:14:57 -0600

    add task works

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -10,7 +10,8 @@
 
 @interface ViewController () <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *textField;
-
+@property NSMutableArray *cells;
+@property (weak, nonatomic) IBOutlet UITableView *taskTableView;
 
 
 @end
@@ -19,20 +20,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.cells = [NSMutableArray new];
 }
 
 -(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
-    return 0;
-    
+    return self.cells.count;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
-    return nil;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellID"];
+    cell.textLabel.text = [self.cells objectAtIndex:indexPath.row];
+    return cell;
 }
 - (IBAction)onAddPressed:(UIButton *)sender {
-
+    [self.cells addObject:self.textField.text];
+    [self.taskTableView reloadData];
+    
 }
 
 @end
