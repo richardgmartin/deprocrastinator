20decf6b463872338a251bee5741e73beaf780a6
richardgmartin <richardgeorgemartin@gmail.com>
2016-01-18 13:25:51 -0600

    first commit

---
+++ Deprocrastinator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Deprocrastinator
+//
+//  Created by Richard Martin on 2016-01-18.
+//  Copyright © 2016 Richard Martin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
