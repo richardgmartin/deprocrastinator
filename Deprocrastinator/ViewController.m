9fbb2af810c81ff8b98680c9dabc7d2e12995bc6
Andy Palka <andrewmpalka@gmail.com>
2016-01-18 16:15:42 -0600

    green tap select acced

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -8,10 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController () <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate>
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UIGestureRecognizerDelegate, UIGestureRecognizerDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property NSMutableArray *cells;
 @property (weak, nonatomic) IBOutlet UITableView *taskTableView;
+@property (strong, nonatomic) IBOutlet UITapGestureRecognizer *tapGestureRecognizer;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *addButton;
 
 
 @end
@@ -22,12 +24,13 @@
     [super viewDidLoad];
     self.cells = [NSMutableArray new];
 }
-- (IBAction)onCellTapped:(UITapGestureRecognizer *)sender {
-    
+
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView cellForRowAtIndexPath:indexPath].textLabel.textColor = [UIColor greenColor];
 }
 
 -(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    
     return self.cells.count;
 }
 
@@ -37,11 +40,14 @@
     cell.textLabel.text = [self.cells objectAtIndex:indexPath.row];
     return cell;
 }
+
 - (IBAction)onAddPressed:(UIButton *)sender {
-    [self.cells addObject:self.textField.text];
-    [self.taskTableView reloadData];
-    [self.textField resignFirstResponder];
-    self.textField.text = @"";
+    if (self.textField.text.length > 0) {
+        [self.cells addObject:self.textField.text];
+        [self.taskTableView reloadData];
+        [self.textField resignFirstResponder];
+        self.textField.text = @"";
+    }
 }
 
 @end
