9f0acd66100ae1247922213237f239f90693fcb7
richardgmartin <richardgeorgemartin@gmail.com>
2016-01-18 15:36:37 -0600

    completed adding new task

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -12,6 +12,9 @@
 
 @property (weak, nonatomic) IBOutlet UITextField *taskToAdd;
 
+@property NSMutableArray *tasksArray;
+
+@property (weak, nonatomic) IBOutlet UITableView *addTableView;
 
 @end
 
@@ -20,24 +23,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
+    self.tasksArray = [NSMutableArray new];
     
 }
 
 -(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
-    return 0;
+    return self.tasksArray.count;
     
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
-    return nil;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellID"];
+    
+    cell.textLabel.text = [self.tasksArray objectAtIndex:indexPath.row];
+    
+    return cell;
 }
 
 - (IBAction)onAddButtonPressed:(UIButton *)sender {
     
+    [self.tasksArray addObject:self.taskToAdd.text];
     
+    [self.addTableView reloadData];
+    
+    [self.taskToAdd resignFirstResponder];
+    
+    self.taskToAdd.text = @"";
+
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
+    return true;
     
 }
 
