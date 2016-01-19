1c1503673ca23d866acf48bf60d71944458ed302
richardgmartin <richardgeorgemartin@gmail.com>
2016-01-18 18:53:48 -0600

    added swipe gesture to change cell item color

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -27,6 +27,27 @@
     
 }
 
+- (IBAction)onSwipeGesture:(UISwipeGestureRecognizer *)sender {
+    
+    if (sender.state == UIGestureRecognizerStateEnded) {
+        
+        CGPoint swipeLocation = [sender locationInView:self.addTableView];
+        NSIndexPath *swipedIndexPath = [self.addTableView indexPathForRowAtPoint:swipeLocation];
+        UITableViewCell *swipedCell = [self.addTableView cellForRowAtIndexPath:swipedIndexPath];
+        
+        if (swipedCell.textLabel.textColor == [UIColor blackColor]) {
+            swipedCell.textLabel.textColor = [UIColor redColor];
+        } else if (swipedCell.textLabel.textColor == [UIColor redColor]) {
+            swipedCell.textLabel.textColor = [UIColor yellowColor];
+        } else if (swipedCell.textLabel.textColor == [UIColor yellowColor]) {
+            swipedCell.textLabel.textColor = [UIColor greenColor];
+        } else {
+            swipedCell.textLabel.textColor = [UIColor blackColor];
+        }
+    }
+}
+
+
 -(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
     return self.tasksArray.count;
@@ -40,6 +61,21 @@
     cell.textLabel.text = [self.tasksArray objectAtIndex:indexPath.row];
     
     return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+    [tableView cellForRowAtIndexPath:indexPath].textLabel.textColor = [UIColor greenColor];
+
+    
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    [self.tasksArray removeObject:self.tasksArray[indexPath.row]];
+    [self.addTableView reloadData];
+    
 }
 
 - (IBAction)onAddButtonPressed:(UIButton *)sender {
@@ -60,6 +96,16 @@
     
 }
 
+- (IBAction)onEditButtonPressed:(UIBarButtonItem *)sender {
+    
+    if (self.editing == NO) {
+        [self.addTableView setEditing:YES];
+        [sender setTitle:@"Done"];
+    } else {
+        [sender setTitle:@"Edit"];
+        [self.addTableView setEditing:NO];
+    }
+}
 
 
 @end
